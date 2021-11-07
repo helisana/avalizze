d512bda1a551578e80d9c3985d63f7a6420af9f4
Heli <helisana.lula@pucpr.edu.br>
2021-11-07 00:01:38 -0300

    atualizados produtos da hoem

--- resources/js/home.js
+++ resources/js/home.js
@@ -3,19 +3,94 @@ function setProdutos() { //armazena
   if (!localStorage.getItem("produtos")) {
     let produtos = [
       {
+        id: 1,
         nome: "Vestido Folhado Simples",
-        preco: "R$ 89,90",
+        preco: "R$ 119,90",
         foto: "resources/img/vestido-azul.webp",
       },
       {
+        id: 2,
         nome: "Camisa Jeans Essential",
         preco: "R$ 189,90",
         foto: "resources/img/camisa.webp",
       },
       {
+        id: 3,
         nome: "Shorts Sarja Essential",
         preco: "R$ 119,90",
         foto: "resources/img/shorts.webp",
+      },
+      {
+        id: 4,
+        nome: "Vestido Branco Lasie",
+        preco: "R$ 289,90",
+        foto: "resources/img/vestido-branco.webp",
+      },
+      {
+        id: 5,
+        nome: "Vestido Preto Cortes",
+        preco: "R$ 299,90",
+        foto: "resources/img/vestido-preto.webp",
+      },
+      {
+        id: 6,
+        nome: "Saia Ruffle Leopardo",
+        preco: "R$ 99,90",
+        foto: "resources/img/saia.webp",
+      },
+      {
+        id: 7,
+        nome: "Blazer Borgonha",
+        preco: "R$ 359,90",
+        foto: "resources/img/blazer.webp",
+      },
+      {
+        id: 8,
+        nome: "Calça Verona Branca",
+        preco: "R$ 249,90",
+        foto: "resources/img/calca.webp",
+      },
+      {
+        id: 9,
+        nome: "Saia Boho",
+        preco: "R$ 89,90",
+        foto: "resources/img/saia2.webp",
+      },
+      {
+        id: 10,
+        nome: "Conjunto Valeska",
+        preco: "R$ 239,90",
+        foto: "resources/img/conjunto.webp",
+      },
+      {
+        id: 11,
+        nome: "Vestido Tianna",
+        preco: "R$ 149,90",
+        foto: "resources/img/vestido-marrom.webp",
+      },
+      {
+        id: 12,
+        nome: "Pantalona Borgonha",
+        preco: "R$ 89,90",
+        foto: "resources/img/pantalona.webp",
+      },
+      {
+        id: 13,
+        nome: "Calça Xadrez Selenna",
+        preco: "R$ 239,90",
+        foto: "resources/img/calca2.webp",
+      },
+      {
+        id: 14,
+        nome: "Camisa Malline Branca",
+        preco: "R$ 249,90",
+        foto: "resources/img/camisa2.webp",
+      },
+      {
+        id: 15,
+        nome: "Sueter Rochedo",
+        preco: "R$ 129,90",
+        foto: "resources/img/blusa2.webp",
       },
     ]  
 
